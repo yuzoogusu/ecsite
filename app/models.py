472e7702dc6ea9177c75ccf57bbce3e53ac98c4f
yuzoogusu <syouta2970@gmail.com>
2021-04-28 11:10:49 +0900

    10:00 admin Item

--- app/models.py
+++ app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+    title = models.CharField(max_length=100)
+    price = models.IntegerField()
+    category = models.CharField(max_length=100)
+    slug = models.SlugField() 
+    dedcription = models.TextField()
+    image = models.ImageField(upload_to='images')
+
+    def __str__(self):
+        return self.title
